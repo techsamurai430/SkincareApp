9b324d12a57d98d17d57dccd58427445923c1be6
kleviscoding <klevisolutions@gmail.com>
2018-04-17 22:19:17 -0700

    Updated and fixed stupid errors

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/components/home';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Home />
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Shake shake shake your booty to open the developer menu.</Text>
       </View>
     );
   }
