98ad1a7af528ced2072eaeb913031aed3688c44f
kleviscoding <klevisolutions@gmail.com>
2018-04-19 02:09:32 -0700

    updates to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,6 @@ export default class Home extends React.Component {
     return (
       <ScrollView>
         <View style={styles.viewStyle}>
-          <View style={{ width: 100, height: 50, backgroundColor: 'powderblue' }} />
-        <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
         <Text style={styles.textStyle}>Home</Text>
         <ProductList />
         </View>
