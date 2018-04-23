c98797aa6b1286dd47b50ddb40b43ce516e30f51
kleviscoding <klevisolutions@gmail.com>
2018-04-23 00:30:01 -0700

    time to stop and look at it in the morning

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { ScrollView, View, Image } from 'react-native';
 import { Text, Button, Row } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
 export default class About extends React.Component {
   render() {
     return (
+      <ScrollView>
         <View style={styles.viewStyle}>
         <Image
           source={{ uri: 'https://static.wixstatic.com/media/dbda37_b4905467220149a392ae67875a217abf~mv2.png/v1/fill/w_381,h_132,al_c,usm_0.66_1.00_0.01/dbda37_b4905467220149a392ae67875a217abf~mv2.png' }}
@@ -51,6 +52,7 @@ export default class About extends React.Component {
               </Button>
               </Row>
         </View>
+        </ScrollView>
 );
 }
 }
@@ -58,9 +60,9 @@ export default class About extends React.Component {
    viewStyle: {
      backgroundColor: 'black',
      alignItems: 'center',
-     height: 700,
+     height: 600,
      padding: 10,
-     elevation: 1,
+     elevation: 2,
      position: 'relative'
    },
    textStyle: {
