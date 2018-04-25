cf68ae7c1608ee27204e9cd12fa890130bdc5b5f
kleviscoding <klevisolutions@gmail.com>
2018-04-25 11:09:50 -0700

    created new login page and add new product page

--- src/components/Addproduct.js
+++ src/components/Addproduct.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
-import { Container, Content, Form, Item, Input, Label } from 'native-base';
+import { Container, Content, Form, Item, Input, Label, Button, Text } from 'native-base';
+import { Actions } from 'react-native-router-flux';
 
 export default class Addproduct extends Component {
   render() {
     return (
       <Container>
-        <Content>
+        <Content padding>
           <Form>
             <Item stackedLabel>
-              <Label>Username</Label>
+              <Label>Name of Product</Label>
+              <Input />
+            </Item>
+            <Item stackedLabel>
+              <Label>Url of Product Image</Label>
+              <Input />
+            </Item>
+            <Item stackedLabel>
+              <Label>Product Description</Label>
+              <Input />
+            </Item>
+            <Item stackedLabel>
+              <Label>Price of Product</Label>
               <Input />
             </Item>
             <Item stackedLabel last>
-              <Label>Password</Label>
+              <Label>Product Page Url</Label>
               <Input />
             </Item>
+            <Button
+              Button block success
+              onPress={() => { Actions.Addproduct(); }}
+            >
+            <Text>Submit Product</Text>
+          </Button>
           </Form>
         </Content>
       </Container>
--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ export default class Login extends Component {
   render() {
     return (
       <Container>
-        <Content>
+        <Content padding>
           <Form>
             <Item stackedLabel>
               <Label>Username</Label>
