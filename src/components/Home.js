92e118ab3a7f3a3e309d7298c1ca88d7dd95a334
kleviscoding <klevisolutions@gmail.com>
2018-05-09 14:58:34 -0700

    making progress on redesign

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,18 +10,17 @@ export default class Home extends React.Component {
         <View style={styles.viewStyle}>
           <Image
             source={{ uri: 'https://static.wixstatic.com/media/dbda37_b4905467220149a392ae67875a217abf~mv2.png/v1/fill/w_381,h_132,al_c,usm_0.66_1.00_0.01/dbda37_b4905467220149a392ae67875a217abf~mv2.png' }}
-            style={{ width: 160, height: 60, padding: 5 }}
+            style={{ width: 160, height: 60, padding: 10 }}
           />
           <Text style={styles.textStyle}>Cannabis Infused Luxury Skincare
           </Text>
 
           <Image
             source={{ uri: 'https://scontent.fphx1-1.fna.fbcdn.net/v/t1.0-9/31225138_10156315166967236_1786995132902408192_n.jpg?_nc_cat=0&oh=2adf63a7482c0614dc4a84993d2f03eb&oe=5B50D150' }}
-            style={{ width: 315, height: 175, margin: 5 }}
+            style={{ width: 315, height: 180, margin: 5 }}
           />
           <Text
-              style={{ color: '#ab8321', fontSize: 12, fontWeight: 'bold'
-             }}
+              style={{ color: '#ab8321', fontSize: 12, fontWeight: 'bold' }}
               onPress={() => Linking.openURL('http://www.420-Skincare.com')}
           >
             420-Skincare.com
@@ -29,18 +28,18 @@ export default class Home extends React.Component {
           <Text style={styles.textStyle2}>
             From Infused Body Butter to CBD Soaps,
             we specialize in Natural and Organic, Hand-Crafted
-            Skincare for all Skin Types. 420 Skincare products will make
-            you feel good inside and out!
+            Skincare for all Skin Types.
             Our soothing all-natural ingredients can improve several
             skin conditions. 50 percent of our proceeds go directly
-            back into Start Living Recovery Home. For more information
-            on 420 Skincare please visit our website.
+            back into Start Living Recovery Home. 420 Skincare products
+            will make you feel good inside and out! For more information
+            please visit our website.
           </Text>
 
           <Row>
           <Button
             Button small rounded success
-            style={{ margin: 5 }}
+            style={{ margin: 10 }}
             onPress={() => { Actions.Products(); }}
           >
           <Text>420 Skincare Products</Text>
@@ -61,13 +60,13 @@ export default class Home extends React.Component {
      position: 'relative'
    },
    textStyle: {
-     fontSize: 16,
-     padding: 5,
+     fontSize: 17,
+     padding: 10,
      fontWeight: 'bold',
      color: '#ab8321'
    },
    textStyle2: {
-     fontSize: 15,
+     fontSize: 14,
      padding: 10,
      fontWeight: 'bold',
      color: '#ab8321'
