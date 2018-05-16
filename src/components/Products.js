d1a037bd5da9e45fc4018ef130929e04c73879c9
kleviscoding <klevisolutions@gmail.com>
2018-05-16 00:20:51 -0700

    last commit of the night

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, View, Image } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import ProductList from './ProductList';
 
 export default class Products extends React.Component {
@@ -7,15 +7,7 @@ export default class Products extends React.Component {
     return (
       <View>
         <ScrollView>
-          <Image
-            source={require('./420skincare_logo.png')}
-            style={{ width: 100, height: 50, alignSelf: 'center' }}
-          />
           <ProductList />
-          <Image
-            source={require('./420skincare_logo.png')}
-            style={{ width: 100, height: 50, alignSelf: 'center' }}
-          />
         </ScrollView>
       </View>
      );
