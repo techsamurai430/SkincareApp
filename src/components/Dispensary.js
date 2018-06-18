1b2ba329f9a312540c140974a84b1db7b9e9f567
kleviscoding <klevisolutions@gmail.com>
2018-06-17 23:26:29 -0700

    updates to dispensary page

--- src/components/Dispensary.js
+++ src/components/Dispensary.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { ScrollView, View, Image, Linking } from 'react-native';
-import { Text, Button, Row } from 'native-base';
-// import { Actions } from 'react-native-router-flux';
+import { Text } from 'native-base';
+
 
 export default class Dispensary extends Component {
   render() {
@@ -17,6 +17,12 @@ export default class Dispensary extends Component {
             THC please visit these dispensaries.
             You must have an AZ MMJ card to purchase.
             (Click for website)
+          </Text>
+
+          <Text
+            style={styles.textStyle}
+          >
+              Phoenix Dispensares
           </Text>
 
           <Text
@@ -94,7 +100,7 @@ export default class Dispensary extends Component {
           >
               Tucson Dispensares
           </Text>
-              
+
           <Text
             style={styles.textStyle2}
             onPress={() => Linking.openURL('https://botanica.us/')}
@@ -109,12 +115,6 @@ export default class Dispensary extends Component {
             PURPLE MED
           </Text>
 
-          <Text
-              style={{ color: '#ab8321', fontSize: 13, padding: 3, fontWeight: 'bold' }}
-              onPress={() => Linking.openURL('http://www.420-Skincare.com')}
-          >
-            420-Skincare.com
-          </Text>
         </View>
       </ScrollView>
     );
