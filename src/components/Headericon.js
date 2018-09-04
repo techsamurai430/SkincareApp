8f71ec2e0617082820cd0849e4d78d689e562d5f
kleviscoding <klevisolutions@gmail.com>
2018-09-04 16:36:24 -0700

    trying to update logo in headericon component

--- src/components/Headericon.js
+++ src/components/Headericon.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { Image, View } from 'react-native';
 import { Header } from 'native-base';
 
+const logoimage = require('./420skincare_logo.png');
+
 export default class HeaderIcon extends Component {
   render() {
     return (
     <View>
       <Header transparent>
         <Image
-            source={require('./420skincare_logo.png')}
+            source={logoimage}
             style={{
             height: 100,
             width: 100,
