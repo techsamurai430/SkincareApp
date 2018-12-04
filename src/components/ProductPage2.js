f208964350b4d53f3036f2ef1302f33127231127
kleviscoding <klevisolutions@gmail.com>
2018-12-04 13:41:27 -0700

    added product detail pages

---
+++ src/components/ProductPage2.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { WebView } from 'react-native';
+
+export default class ProductPage2 extends Component {
+  render() {
+    return (
+      <WebView
+         source={{ uri: 'https://www.420-skincare.com/product-page/cbd-bath-bomb-dust' }}
+      />
+    );
+  }
+}
---
+++ src/components/ProductPage4.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { WebView } from 'react-native';
+
+export default class ProductPage4 extends Component {
+  render() {
+    return (
+      <WebView
+         source={{ uri: 'hhttps://www.420-skincare.com/product-page/body-butter' }}
+      />
+    );
+  }
+}
